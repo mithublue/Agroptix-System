34a202e8eef75125373f334c58da732d89e48890
Mithu <cemithu06@gmail.com>
2025-06-21 05:55:22 +0600

    view

--- resources/views/source/index.blade.php
+++ resources/views/source/index.blade.php
@@ -1,7 +1,19 @@
-{{--
     @extends('layouts.app')
 
     @section('content')
-        source.index template
+        @if (session('success'))
+            <div
+                x-data="{ show: true }"
+                x-show="show"
+                x-transition
+                x-init="setTimeout(() => show = false, 3000)"
+                class="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50"
+                role="alert"
+            >
+                <div class="flex justify-between">
+                    <span class="font-medium">{{ 'Hello world !' }}</span>
+                    <button @click="show = false" type="button" class="font-bold">X</button>
+                </div>
+            </div>
+        @endif
     @endsection
---}}
