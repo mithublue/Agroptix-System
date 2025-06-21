01ec5b27a9fc0bf4ae24220c90fa331807379b4b
Mithu <cemithu06@gmail.com>
2025-06-21 06:14:03 +0600

    view

--- resources/views/source/index.blade.php
+++ resources/views/source/index.blade.php
@@ -1,19 +1,21 @@
-    @extends('layouts.app')
+<x-app-layout>
+    {{-- This optional header slot will go into the <header> of your layout --}}
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Sources') }}
+        </h2>
+    </x-slot>
 
-    @section('content')
-        @if (session('success'))
-            <div
-                x-data="{ show: true }"
-                x-show="show"
-                x-transition
-                x-init="setTimeout(() => show = false, 3000)"
-                class="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50"
-                role="alert"
-            >
-                <div class="flex justify-between">
-                    <span class="font-medium">{{ 'Hello world !' }}</span>
-                    <button @click="show = false" type="button" class="font-bold">X</button>
+    {{-- This is the main content that will go into the {{ $slot }} variable --}}
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+
+                    <p>Your list of sources and other content goes here!</p>
+
                 </div>
             </div>
-        @endif
-    @endsection
+        </div>
+    </div>
+</x-app-layout>
