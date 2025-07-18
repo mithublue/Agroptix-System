3177e63c7226fa6b8437f41ab846cba23eaabb28
Mithu <cemithu06@gmail.com>
2025-07-18 21:51:01 +0600

    menu text

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -167,7 +167,7 @@
                                 <svg class="w-5 h-5 mr-3 {{ request()->is('shipments*') ? 'text-indigo-500' : 'text-gray-400' }}" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2"></path>
                                 </svg>
-                                Shipments
+                                Packaging
                             </div>
                             <svg :class="{'transform rotate-180': open}" class="w-4 h-4 text-gray-500 transition-transform duration-200" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
