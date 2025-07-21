f95024ae31625a3591c5d7f30611c7ba098fd8d0
Mithu <cemithu06@gmail.com>
2025-07-21 13:22:31 +0600

    Fix

--- resources/views/rpcunit/index.blade.php
+++ resources/views/rpcunit/index.blade.php
@@ -131,7 +131,7 @@
                 </div>
                 <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
                     <button type="button"
-                            @click="closeModal()"
+                            @click="close()"
                             class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:ml-3 sm:w-auto sm:text-sm">
                         Close
                     </button>
